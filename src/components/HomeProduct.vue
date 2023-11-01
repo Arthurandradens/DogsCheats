5ae6c9ad7837e344ca774f8e6870ed494b46830d
Arthurandradens <nunesarthur252@gmail.com>
2023-11-01 12:00:53 -0300

    new commit

--- src/components/HomeProduct.vue
+++ src/components/HomeProduct.vue
@@ -1,135 +1,54 @@
 <template>
   <v-container>
-          <h1>Dashboard</h1>
-  
-          <v-row>
-            <v-col cols="12" sm="6" md="4" lg="3">
-              <v-card flat class="border fill-height">
-                <v-img
-                  class="align-end text-white"
-                  src="@/assets/golden.jpg"
-                  height="200px"
-                >
-                  <v-card-title>Golden puppy</v-card-title>
-                </v-img>
-  
-                <v-card-subtitle class="pt-3">learn all about them</v-card-subtitle>
-  
-                <v-card-text>
-                  <div>Lorem ipsum dolor sit amet</div>
-                </v-card-text>
-  
-                <v-card-actions>
-                  <v-btn variant="outlined" color="primary" >See more</v-btn>
-                  <v-btn prepend-icon="mdi-cart" variant="tonal" color="error">Buy</v-btn>
-                  <!-- <v-btn @click="toggleStar(0)" :icon="items[0].star" variant="" color="error"></v-btn> -->
-                </v-card-actions>
-              </v-card>
-            </v-col>
-  
-            <v-col cols="12" sm="6" md="4" lg="3">
-              <v-card flat class="border fill-height">
-                <v-img
-                  class="align-end text-white"
-                  src="@/assets/golden.jpg"
-                  height="200px"
-                >
-                  <v-card-title>Golden puppy</v-card-title>
-                </v-img>
-  
-                <v-card-subtitle class="pt-3">learn all about them</v-card-subtitle>
-  
-                <v-card-text>
-                  <div>Lorem ipsum dolor sit amet</div>
-                </v-card-text>
-  
-                <v-card-actions>
-                  <v-btn variant="outlined" color="primary" >See more</v-btn>
-                  <v-btn prepend-icon="mdi-cart" variant="tonal" color="error">Buy</v-btn>
-                  <!-- <v-btn @click="toggleStar(1)" :icon="items[1].star" variant="" color="error"></v-btn> -->
-                </v-card-actions>
-              </v-card>
-            </v-col>
-  
-            <v-col cols="12" sm="6" md="4" lg="3">
-              <v-card flat class="border fill-height">
-                <v-img
-                  class="align-end text-white"
-                  src="@/assets/golden.jpg"
-                  height="200px"
-                >
-                  <v-card-title>Golden puppy</v-card-title>
-                </v-img>
-  
-                <v-card-subtitle class="pt-3">learn all about them</v-card-subtitle>
-  
-                <v-card-text>
-                  <div>Lorem ipsum dolor sit amet</div>
-                </v-card-text>
-  
-                <v-card-actions>
-                  <v-btn variant="outlined" color="primary" >See more</v-btn>
-                  <v-btn prepend-icon="mdi-cart" variant="tonal" color="error">Buy</v-btn>
-                  <!-- <v-btn @click="toggleStar(2)" :icon="items[2].star" variant="" color="error"></v-btn> -->
-                </v-card-actions>
-              </v-card>
-            </v-col>
-  
-            <v-col cols="12" sm="6" md="4" lg="3">
-              <v-card flat class="border fill-height">
-                <v-img
-                  class="align-end text-white"
-                  src="@/assets/golden.jpg"
-                  height="200px"
-                >
-                  <v-card-title>Golden puppy</v-card-title>
-                </v-img>
-  
-                <v-card-subtitle class="pt-3">learn all about them</v-card-subtitle>
-  
-                <v-card-text>
-                  <div>Lorem ipsum dolor sit amet</div>
-                </v-card-text>
-  
-                <v-card-actions>
-                  <v-btn variant="outlined" color="primary" >See more</v-btn>
-                  <v-btn prepend-icon="mdi-cart" variant="tonal" color="error">Buy</v-btn>
-                  <!-- <v-btn @click="toggleStar(3)" :icon="items[3].star" variant="" color="error"></v-btn> -->
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+    <h1>Dashboard</h1>
+    <v-row>
+      <v-col v-for="(character, id) in this.products" :key="id" cols="12" sm="6" md="4" lg="3">
+        <v-card flat class="border fill-height">
+          <v-img class="align-end text-white" :src="character.image" height="200px">
+            <v-card-title> {{ character.name }}</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pt-3">learn all about them</v-card-subtitle>
+
+          <v-card-text>
+            <div>Lorem ipsum dolor sit amet</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn variant="outlined" color="primary">See more</v-btn>
+            <v-btn prepend-icon="mdi-cart" variant="tonal" color="error">Buy</v-btn>
+            <!-- <v-btn @click="toggleStar(0)" :icon="items[0].star" variant="" color="error"></v-btn> -->
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script setup>
-import router from '@/router';
-import axios from "axios";
-import { ref } from 'vue';
+<script>
+import axios from 'axios';
 
-/**
- * Data
- */
-const nome = ref(null);
-  
-  /**
-   * Methods
-   */
-  // const navegar = () => {
-  //   router.push('/page-two')
-  // }
-
-  // const axios = require('axios');
-  const chamaAPI = () => {
-    axios.get('https://rickandmortyapi.com/api/character')
-    .then((response) => {
-      for(let i = 0; i < 10 ; i++){
-        console.log(response.data.results[i].name);
+export default {
+  data() {
+    return {
+      products: [],
+      response: null,
+    };
+  },
+  methods: {
+    async getUsersImage() {
+      try {
+        this.response = await axios.get('https://rickandmortyapi.com/api/character');
+        this.products = this.response.data.results;
+        
+        
+      } catch (error) {
+        console.error(error);
       }
-    })
-    .catch((error) => {
-      console.log(error.data)
-    })
-    .finally()
-  };
+    },
+  },
+  created() {
+    this.getUsersImage();
+  },
+};
 </script>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,17 +1,41 @@
 <template>
     <v-container>
-      <v-row>
-        <v-col
-          v-for="name in characterNames"
-        
-          cols="12"
-      
-        >
+    <v-card>
+      <div class="d-flex justify-space-between">
+        <v-card-title>--Users List--</v-card-title>
+        <v-dialog width="500px">
+          <template #activator="{ props }"> 
+            <v-btn v-bind="props" class="mt-4 mr-4" variant="tonal" size="small"> New User </v-btn>
+          </template>
           <v-card>
-            <v-card-title>{{ name }}</v-card-title>
+            <v-card-title>teste</v-card-title>
           </v-card>
-        </v-col>
-      </v-row>
+        </v-dialog>
+      </div>
+      <v-table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            v-for="(user,index) in characterNames"
+            :key="index"
+             cols="12">
+            <td>{{ user.name }}</td>
+            <td> {{ user.status }} </td>
+            <td>
+              <v-btn variant="tonal" color="primary">Edit</v-btn>
+              <v-btn class="ml-2" variant="tonal" color="error">Delete</v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-card>
     </v-container>
   </template>
   
@@ -20,25 +44,48 @@
   import axios from 'axios';
   
   export default {
-    setup() {
-      const characterNames = ref([]);
+    // setup() {
+    //   const characterNames = ref([]);
+
+    //   const getUsers = async () => {
+    //     try {
+    //       const response = await axios.get('https://rickandmortyapi.com/api/character');
+    //       for (let i = 0; i < 10; i++) {
+    //         characterNames.value.push(response.data.results[i]);
+    //       }
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   };
   
-      const getUsers = async () => {
-        try {
-          const response = await axios.get('https://rickandmortyapi.com/api/character');
-          for (let i = 0; i < 10; i++) {
-            characterNames.value.push(response.data.results[i].name);
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
+    //   onMounted(getUsers);
   
-      onMounted(getUsers);
-  
-      return {
-        characterNames,
-      };
+    //   return {
+    //     characterNames,
+    //   };
+    // },
+    data(){
+      return{
+         characterNames: [],
+         response: null
+      }
     },
-  };
+    methods: {
+     async getUsers(){
+        try{
+          this.response = await axios.get('https://rickandmortyapi.com/api/character');
+          for(let i = 0; i < 10; i++){
+            this.characterNames.push(this.response.data.results[i]);
+        }
+      }
+      catch(error){
+            console.error(error)
+          }
+    }
+  },
+
+  created(){
+    this.getUsers()
+  }
+}
   </script>
